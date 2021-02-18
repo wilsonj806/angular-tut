61535a7cb3e5d6fe17e8998b55f2853adf948ecf
Wilson <wilsonj806@gmail.com>
2021-02-18 17:20:42 -0500

    Convert hero string to an object

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Heroes } from './heroes'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Shinra Kusakabe'
+  hero: Heroes = {id: 1, name:'Shinra Kusakabe'}
 
   constructor() { }
 
